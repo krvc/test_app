c2586372a1e6546c9b2a2ec0072c332a67911a3e
Rakesh Vidya Chandra <krvc.it@gmail.com>
2013-03-01 18:14:20 +0530

    final commit

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,11 @@ from django import forms
 class ClientForm(forms.Form):
     client_id = forms.CharField(max_length=124)
     client_secret = forms.CharField(max_length=124)
+
+
+class ResumeUploadForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    projects_url = forms.URLField()
+    code_url = forms.URLField()
+    resume = forms.FileField()
--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@
 
 from django.shortcuts import render_to_response ,redirect
 from django.template import RequestContext
-from app.forms import ClientForm
-import urllib2
-import urllib
-import json
+from django.core.context_processors import csrf
+from app.forms import ClientForm, ResumeUploadForm
+import urllib, urllib2
+import json, sys
 
 
 def home(request):
@@ -14,27 +14,28 @@ def home(request):
         if form.is_valid():
             global client_id
 	    global client_secret
+            global redirect_uri
+            redirect_uri="http://127.0.0.1:8000/callback"
 	    client_id=request.POST['client_id']
 	    client_secret=request.POST['client_secret']
-	    return redirect('http://join.agiliq.com/oauth/authorize?client_id='+client_id+'&redirect_uri=http://127.0.0.1:8000/callback')
+	    return redirect('http://join.agiliq.com/oauth/authorize?client_id='+client_id+'&redirect_uri='+redirect_uri, request_context=RequestContext(request))
     else:
         form = ClientForm()
         return render_to_response('home.html', {'form':form }, context_instance=RequestContext(request))
 
+
 def callback(request):
 	code = request.GET['code']
-	url = 'http://join.agiliq.com/oauth/access_token/'
+	url = 'http://join.agiliq.com/oauth/access_token/?'
 	values = {'client_id' : client_id,
-			  'redirect_uri' : 'http://localhost:8000/agiliq',
+			  'redirect_uri' : redirect_uri,
 			  'client_secret' : client_secret,
-			  'code': code,}
-
-
+			  'code': code, }
 	req = urllib2.Request(url+urllib.urlencode(values))
 	response = urllib2.urlopen(req)
-	the_page = response.read()
-	obj = json.loads(the_page)
+	obj = json.loads(response.read())
 	token=obj['access_token']
+        form = ResumeUploadForm()
 	return render_to_response('upload.html', {
-	    'access_token': token,
-	},)
+	   'access_token': token, 'form':form,
+	}, context_instance=RequestContext(request))
