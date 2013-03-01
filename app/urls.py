f605a6513244f27104c8ef7a078945c84beb8e45
Rakesh Vidya Chandra <krvc.it@gmail.com>
2013-03-01 15:50:48 +0530

    modified app/urls

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
+    url(r'^callback$', 'app.views.callback', name='callback'),
 )
