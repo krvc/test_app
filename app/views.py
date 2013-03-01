6e189315b1cc71173337de768e6b51b6489c9cd1
Rakesh Vidya Chandra <krvc.it@gmail.com>
2013-03-01 12:34:57 +0530

    modified 'index' view to render form with context

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from app.forms import ClientForm
 
 def home(request):
-    return render_to_response('home.html')
+    form = ClientForm()
+    return render_to_response('home.html', {'form':form }, context_instance=RequestContext(request))
